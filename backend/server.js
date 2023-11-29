fcd24042a9b4c213da8d76832e17cab622b64121
Eyran07 <eyranabbou20@gmail.com>
2023-11-29 17:10:59 +0200

    d

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
-const authRoutes = require('./routes/user');
+const userRoutes = require('./routes/user');
 
 const app = express();
 
 // Middleware
 app.use(cors());
 app.use(express.json()); // Pour parser le JSON
-app.use('/', authRoutes);
+app.use('/', userRoutes);
 
 // Connection à MongoDB
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
--- frontend/src/pages/Admin.jsx
+++ frontend/src/pages/Admin.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import {
   Table,
   Thead,
@@ -10,15 +10,26 @@ import {
   TableContainer,
   Button,
   Input,
+  Box,
+  Heading,
   Switch,
-} from '@chakra-ui/react';
+  HStack
+} from "@chakra-ui/react";
 
 const Admin = () => {
   const [users, setUsers] = useState([]);
   const [editingUserId, setEditingUserId] = useState(null);
-  const [editedUser, setEditedUser] = useState({ name: '', password: '', admin: false });
+  const [editedUser, setEditedUser] = useState({
+    name: "",
+    password: "",
+    admin: false,
+  });
   const [addingNew, setAddingNew] = useState(false);
-  const [newUser, setNewUser] = useState({ name: '', password: '', admin: false });
+  const [newUser, setNewUser] = useState({
+    name: "",
+    password: "",
+    admin: false,
+  });
 
   const fetchUsers = () => {
     axios
@@ -46,22 +57,24 @@ const Admin = () => {
 
   const saveEdit = (deleteUser = false) => {
     if (deleteUser) {
-      axios.delete(`http://localhost:6060/users/${editingUserId}`)
-        .then(response => {
+      axios
+        .delete(`http://localhost:6060/users/${editingUserId}`)
+        .then((response) => {
           fetchUsers();
           cancelEdit();
         })
-        .catch(error => {
-          console.error('Erreur lors de la suppression', error);
+        .catch((error) => {
+          console.error("Erreur lors de la suppression", error);
         });
     } else {
-      axios.put(`http://localhost:6060/users/${editingUserId}`, editedUser)
-        .then(response => {
+      axios
+        .put(`http://localhost:6060/users/${editingUserId}`, editedUser)
+        .then((response) => {
           fetchUsers();
           cancelEdit();
         })
-        .catch(error => {
-          console.error('Erreur lors de la mise à jour', error);
+        .catch((error) => {
+          console.error("Erreur lors de la mise à jour", error);
         });
     }
   };
@@ -92,77 +105,126 @@ const Admin = () => {
   };
 
   return (
-    <TableContainer>
-      <Table variant="simple">
-        <Thead>
-          <Tr>
-            <Th>Nom</Th>
-            <Th>mot de passe</Th>
-            <Th>Admin</Th>
-            <Th>Modifier</Th>
+    <>
+ <Box
+  display={'flex'}
+  flexDirection="column"
+  justifyContent={'center'}
+  alignContent={'center'}
+  overflowY="auto"
+  mt={'100px'}
+  bg="white" // White background for the main box
+  p={4}
+  borderRadius="md"
+>
+  <Heading mb={6} color="black">
+    Gestion des Utilisateurs
+  </Heading>
+
+  <TableContainer>
+    <Table variant="simple" size="md" style={{ tableLayout: 'fixed' }}>
+      <Thead bgColor="gray.200">
+        <Tr>
+          <Th style={{ width: '25%' }}>Nom</Th>
+          <Th style={{ width: '25%' }}>Mot de passe</Th>
+          <Th style={{ width: '25%' }}>Admin</Th>
+          <Th style={{ width: '25%' }}>Actions</Th>
+        </Tr>
+      </Thead>
+      <Tbody>
+        {users.map((user) => (
+          <Tr key={user._id}>
+            {editingUserId === user._id ? (
+              <>
+                <Td>
+                  <Input value={editedUser.name} onChange={(e) => setEditedUser({...editedUser, name: e.target.value})} />
+                </Td>
+                <Td>
+                  <Input value={editedUser.password} onChange={(e) => setEditedUser({...editedUser, password: e.target.value})} />
+                </Td>
+                <Td>
+                  <Switch isChecked={editedUser.admin} onChange={(e) => setEditedUser({...editedUser, admin: e.target.checked})} />
+                </Td>
+                <Td>
+                  <HStack spacing={2}>
+                    <Button size="sm" colorScheme="blue" onClick={() => saveEdit()}>Valider</Button>
+                    <Button size="sm" colorScheme="red" onClick={() => deleteUser(editingUserId)}>Supprimer</Button>
+                    <Button size="sm" colorScheme="gray" onClick={cancelEdit}>Annuler</Button>
+                  </HStack>
+                </Td>
+              </>
+            ) : (
+              <>
+                <Td>{user.name}</Td>
+                <Td>{user.password}</Td>
+                <Td>
+                  <Switch isChecked={user.admin} isReadOnly />
+                </Td>
+                <Td>
+                  <Button size="sm" colorScheme="green" onClick={() => startEdit(user)}>Modifier</Button>
+                </Td>
+              </>
+            )}
           </Tr>
-        </Thead>
-        <Tbody>
-          {users.map(user => (
-            <Tr key={user._id}>
-              {editingUserId === user._id ? (
-                <>
+        ))}
+              {addingNew && (
+                <Tr>
                   <Td>
-                    <Input value={editedUser.name} onChange={(e) => setEditedUser({ ...editedUser, name: e.target.value })} />
+                    <Input
+                      placeholder="Nom"
+                      value={newUser.name}
+                      onChange={(e) =>
+                        setNewUser({ ...newUser, name: e.target.value })
+                      }
+                    />
                   </Td>
                   <Td>
-                    <Input value={editedUser.password} onChange={(e) => setEditedUser({ ...editedUser, password: e.target.value })} />
+                    <Input
+                      placeholder="Mot de passe"
+                      value={newUser.password}
+                      onChange={(e) =>
+                        setNewUser({ ...newUser, password: e.target.value })
+                      }
+                    />
                   </Td>
                   <Td>
-                    <Switch isChecked={editedUser.admin} onChange={(e) => setEditedUser({ ...editedUser, admin: e.target.checked })} />
+                    <Switch
+                      isChecked={newUser.admin}
+                      onChange={(e) =>
+                        setNewUser({ ...newUser, admin: e.target.checked })
+                      }
+                    />
                   </Td>
                   <Td>
-                    <Button colorScheme="blue" onClick={() => saveEdit()}>Valider</Button>
-                    <Button colorScheme="red" ml={2} onClick={() => deleteUser(editingUserId)}>Supprimer</Button>
-                    <Button colorScheme="gray" ml={2} onClick={cancelEdit}>Annuler</Button>
+                    <Button colorScheme="blue" onClick={saveNewUser}>
+                      Créer
+                    </Button>
+                    <Button
+                      colorScheme="gray"
+                      onClick={() => setAddingNew(false)}
+                    >
+                      Annuler
+                    </Button>
                   </Td>
-                </>
-              ) : (
-                <>
-                  <Td>{user.name}</Td>
-                  <Td>{user.password}</Td>
-                  <Td>
-                    <Switch isChecked={user.admin} isReadOnly />
-                  </Td>
-                  <Td>
-                    <Button colorScheme="green" onClick={() => startEdit(user)}>Modifier</Button>
-                  </Td>
-                </>
+                </Tr>
               )}
-            </Tr>
-          ))}
-          {addingNew && (
-            <Tr>
-              <Td>
-                <Input placeholder="Nom" value={newUser.name} onChange={(e) => setNewUser({ ...newUser, name: e.target.value })} />
-              </Td>
-              <Td>
-                <Input placeholder="Mot de passe" value={newUser.password} onChange={(e) => setNewUser({ ...newUser, password: e.target.value })} />
-              </Td>
-              <Td>
-                <Switch isChecked={newUser.admin} onChange={(e) => setNewUser({ ...newUser, admin: e.target.checked })} />
-              </Td>
-              <Td>
-                <Button colorScheme="blue" onClick={saveNewUser}>Créer</Button>
-                <Button colorScheme="gray" onClick={() => setAddingNew(false)}>Annuler</Button>
-              </Td>
-            </Tr>
-          )}
-          {!addingNew && (
-            <Tr>
-              <Td colSpan={4}>
-                <Button colorScheme="green" onClick={() => setAddingNew(true)}>+</Button>
-              </Td>
-            </Tr>
-          )}
-        </Tbody>
-      </Table>
-    </TableContainer>
+              {!addingNew && (
+                <Tr>
+                  <Td colSpan={4}>
+                    <Button
+                      colorScheme="green"
+                      onClick={() => setAddingNew(true)}
+                    >
+                      +
+                    </Button>
+                  </Td>
+                </Tr>
+              )}
+            </Tbody>
+          </Table>
+        </TableContainer>
+      </Box>{" "}
+    </>
   );
 };
 
